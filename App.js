8303cca4a6f1b7f5aa0a07cbec3c30504bc7178f
samet <sametcn99@gmail.com>
2024-01-19 03:19:49 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const websiteUrl = 'https://next-github-profile-viewer.vercel.app';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView source={{ uri: websiteUrl }} style={styles.container} />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    minHeight: "100vh",
   },
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+# GitHub Explorer for React Native
+
+This React Native application is using webview to render the [GitHub Profile Viewer](https://next-github-profile-viewer.vercel.app/) project's website.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
